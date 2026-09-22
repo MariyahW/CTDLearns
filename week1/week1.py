be27cae619e52948a707aca78b42042b900b11ab
NewAdmin <newadmin@Mariyahs-MacBook.local>
2026-09-22 11:23:36 -0400

    Section 2 completed

--- week1/week1.py
+++ week1/week1.py
@@ -9,3 +9,11 @@ user={
 for value in user.values():
     print(value, type(value))
 
+# Section 2: User Input and Math
+from datetime import date
+current_year = date.today().year
+input_name = input("Hello, it's nice to meet you! What is your name? ")
+input_age = input(f"So, {input_name}! What year were you born? ")
+
+print(f"Hi again {input_name}, by my calculations, you are {current_year - int(input_age)}")
+
