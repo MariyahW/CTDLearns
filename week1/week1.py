2c554a83c967ebe7dc3040f6c882df8340109dc8
NewAdmin <newadmin@Mariyahs-MacBook.local>
2026-09-22 11:26:44 -0400

    Section 3 completed

--- week1/week1.py
+++ week1/week1.py
@@ -15,5 +15,10 @@ current_year = date.today().year
 input_name = input("Hello, it's nice to meet you! What is your name? ")
 input_age = input(f"So, {input_name}! What year were you born? ")
 
-print(f"Hi again {input_name}, by my calculations, you are {current_year - int(input_age)}")
+print(f"Hi again {input_name}, by my calculations, you are {current_year - int(input_age)} years old")
 
+# Section 3: Type Conversion and f-Strings
+num1=input("Pick a number, any number! ")
+num2=input("Pick another number! ")
+result = float(num1) * float(num2)
+print(f"The product of {num1} and {num2} is {result} aka {num1} * {num2} = {result}")
