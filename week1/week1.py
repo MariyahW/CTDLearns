27e75b6307be30587f12136a34f9798893582e23
NewAdmin <newadmin@Mariyahs-MacBook.local>
2026-09-22 11:18:25 -0400

    Section 1 completed

---
+++ week1/week1.py
@@ -0,0 +1,11 @@
+# Section 1:Variables and Types
+user={
+    "name": "Samantha",
+    "age": 11,
+    "height": 3.6,
+    "is_student": False
+}
+
+for value in user.values():
+    print(value, type(value))
+
